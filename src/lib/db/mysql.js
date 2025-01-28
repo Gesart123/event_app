2c812a3c7b137dd73c455b15a903acc9eb1707a6
Gesart Mlloja <gesmll20@htl-shkoder.com>
2025-01-28 12:29:47 +0100

    Add env-File

--- src/lib/db/mysql.js
+++ src/lib/db/mysql.js
@@ -1,14 +1,19 @@
 import mysql from 'mysql2/promise';
+import { DB_HOST,
+		DB_USER,
+		DB_PORT,
+		DB_PASSWORD,
+		DB_NAME } from '$env/static/private';
 
 let connection = null;
 export function createConnection() {
 	if (!connection) {
 		connection = mysql.createConnection({
-			host: 'htl-projekt.com',
-			user: 'gesartmlloja',
-			port: 33060,
-			password: '!Insy_2024$',
-			database: '2024_4bw_gesartmlloja_event_app'
+			host: DB_HOST,
+			user: DB_USER,
+			port: DB_PORT,
+			password: DB_PASSWORD,
+			database: DB_NAME
 		});
 	}
 	return connection;
